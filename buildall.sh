0ae668c39cd7c030d1c53483976a2f5def93975a
Lukas Velikov <velikovlukas@gmail.com>
2020-12-23 13:06:12 -0500

    Rewrite build script

--- buildall.sh
+++ buildall.sh
@@ -1,7 +1,19 @@
 #!/bin/bash
-# If you want to build strace2ds library, just run this script.
-# If you want to build and install, run this script with 'install' argument. Ex: ./buildall install
 
+####################
+# Script variables #
+####################
+install=false
+customDataseriesDir=false
+# TODO: Find a sensible default install directory
+installDir="./build"
+dataseriesDir=""
+
+####################
+# Script functions #
+####################
+
+# Wrapper function for running commands with feedback
 function runcmd
 {
     echo "CMD: $@"
@@ -13,6 +25,55 @@ function runcmd
     fi
 }
 
+# Output usage string (invoked on -h or --help)
+function printUsage
+{
+    (
+    cat << EOF
+Usage: $0 [options...]
+Options:
+    --config-args ARGS     Append ARGS to every ./configure command
+    --install              Install libraries and binaries under /usr/local
+    --install-packages     Automatically use apt-get to install missing packages
+    -h, --help             Print this help message
+EOF
+    ) >&2
+    exit 0
+}
+
+##################
+# Script startup #
+##################
+
+while [[ $# -gt 0 ]]; do
+    key="$1"
+
+    case "${key}" in
+        --dataseries-dir)
+            customDataseriesDir=true
+            shift # past argument
+            dataseriesDir="$1"
+            shift # past value
+            ;;
+        --install)
+            install=true
+            shift # past argument
+            ;;
+        --install-dir)
+            shift # past argument
+            installDir="$1"
+            shift # past argument
+            ;;
+        -h|--help)
+            printUsage
+            shift # past argument
+            ;;
+        *)
+            shift # past argument
+            ;;
+    esac
+done
+
 numberOfCores="$(nproc --all)"
 runcmd /bin/rm -rf build
 runcmd mkdir -p build
@@ -22,10 +83,26 @@ runcmd perl gen-xml-enums.pl
 runcmd cd ../
 runcmd cp -r ./xml build
 runcmd cd build
-runcmd cmake ../
+
+if [[ "${install}" == true ]]; then
+    if [[ "${customDataseriesDir}" == true ]]; then
+        runcmd cmake -DCMAKE_INSTALL_PREFIX:PATH="${installDir}"/strace2ds -DDATASERIES_DIR:PATH="${dataseriesDir}" ../
+    else
+        runcmd cmake -DCMAKE_INSTALL_PREFIX:PATH="${installDir}"/strace2ds ../
+    fi
+else
+    if [[ "${customDataseriesDir}" == true ]]; then
+        runcmd -DDATASERIES_DIR:PATH="${dataseriesDir}" ../
+    else
+        runcmd cmake ../
+    fi
+fi
+
 runcmd make clean
 runcmd make -j"${numberOfCores}"
-# If all you're changing is C/C++ code, just "cd build" and run next two commands.
-if [ "$#" -eq 1 ] && [ "$1" = "install" ]; then
+
+if [[ "${install}" == true ]]; then
     runcmd sudo make -j"${numberOfCores}" install
+else
+    runcmd make -j"${numberOfCores}" install
 fi
